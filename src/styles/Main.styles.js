aeef51d072fbb25925da531ee6ef7d7c81e8aa02
Leandro Finochio <lefinochio@hotmail.com>
2021-02-27 08:11:20 -0300

    Correções menu e imagem inicial

--- src/styles/Main.styles.js
+++ src/styles/Main.styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 1200px;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
--- src/styles/Menu.styles.js
+++ src/styles/Menu.styles.js
@@ -5,7 +5,9 @@ export const Container = styled.div`
   padding: 1rem;
   box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
     0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
-  margin-bottom: 2rem;
+  position: sticky;
+  top: 0;
+  background: var(--background);
 
   div {
     display: flex;
